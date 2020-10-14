d7b2e16600c15d98adeb0d7f9980a2302fa12027
bryano13 <bryano13@hotmail.com>
2020-10-14 03:14:33 -0500

    adding all the files for the static landing page (Bryan flask)

---
+++ Immersion_landing_page/country_list/__init__.py
@@ -0,0 +1,6 @@
+from .country_list import available_languages, countries_for_language
+
+__author__ = "Niels Lemmens"
+__email__ = "draso.odin@gmail.com"
+__version__ = "0.2.1"
+__all__ = ["available_languages", "countries_for_language"]
