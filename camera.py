58298d8f29e246f0e0cf93a883d5768e453ed4bf
AMIRANDA9112 <andreselfm@gmail.com>
2020-10-17 17:44:05 -0500

    change pil image script to cv2 scritp

--- camera.py
+++ camera.py
@@ -30,7 +30,7 @@ class Camera(object):
 
 
         # convert eh base64 string in ascii to base64 string in _bytes_
-        self.to_output.append(binascii.a2b_base64(output_str))
+        self.to_output.append(output_str)
 
     def keep_processing(self):
         while True:
