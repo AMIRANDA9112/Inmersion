eaca706b9cc50426001400ffa37070f52ec96575
AMIRANDA9112 <andreselfm@gmail.com>
2020-10-23 03:34:28 -0500

    upload pdf

--- app.py
+++ app.py
@@ -17,11 +17,28 @@ from utils import base64_to_pil_image, pil_image_to_base64
 app = Flask(__name__)
 
 
-socketio = SocketIO(app)
 UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-camera = Camera(Makeup_artist())
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_data.db'
+db = SQLAlchemy(app)
 app.logger.addHandler(logging.StreamHandler(stdout))
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+socketio = SocketIO(app)
+camera = Camera(Makeup_artist())
+
+class User(db.Model):
+    """Model for Users"""
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    country = db.Column(db.String)
+
+
+db.create_all()
 
 
 @socketio.on('input image', namespace='/test')
@@ -108,7 +125,12 @@ def signin():
             return render_template('login.html', message=message)
         if not password:
             return render_template('login.html', message_p='Please fill out this field.')
-        return render_template('login.html', message_success="Successful login!!")
+
+        user = User.query.filter_by(email=email, password=password).first()
+        if user is None:
+            return render_template("login.html", message_failure="Wrong Credentials. Please Try Again.")
+        else:
+            return render_template('login.html', message_success="Successful login!!")
 
     return render_template('login.html')
 
@@ -117,11 +139,44 @@ def signin():
 def signup():
     countries = dict(countries_for_language('en'))
     if request.method == "POST":
-        name = request.form["name"]
+        req = request.form
+        missing = []
+        for field, input in req.items():
+            if input == "":
+                missing.append(field)
 
-        if name:
-            return render_template('signup.html', name="Please fill out this field.")
+        if missing:
+            feedback = " {}".format(', '.join(missing))
+            return render_template('signup.html', countries=countries,
+                                   feedback=feedback)
+        if req["Email"] != req["Confirm email"]:
+            return render_template('signup.html', countries=countries,
+                                   dont_match="Emails don't match")
+        if req["Password"] != req["Confirm password"]:
+            return render_template('signup.html', countries=countries,
+                                   dont_match="Passwords don't match")
+        """adding data to database in SQLAlquemy"""
+        new_user = User(first_name=req["First name"], last_name=req["Last name"],
+                        email=req["Email"], password=req["Password"],
+                        country=req["countries"])
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return render_template("signup.html", countries=countries,
+                                   feedback="email already exists")
+        finally:
+            db.session.close()
+            return render_template("signup.html", countries=countries,
+                                   success="Successful Registration")
     return render_template('signup.html', countries=countries)
+
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
