92cdb3b382ef458b19bbd906aaf6cbf4929220f8
bryano13 <bryano13@hotmail.com>
2020-11-04 01:49:54 -0500

    engine facial landmarks, mark3 and temp/aboutus.html which is the only one with meaninful changes

--- engine/mark3.py
+++ engine/mark3.py
@@ -8,6 +8,9 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
+dvideo = cv2.VideoCapture('../media/math.mp4')
+
+
 
 
 fn = 0
@@ -16,14 +19,13 @@ fn = 0
 while cap.isOpened():
     fn += 1
 
-    im_src = cv2.imread('../media/icon2.png')
+    ret, im_src = dvideo.read()
     # Capture frame-by-frame
 
     ret, frame = cap.read()
 
     org_frame = frame
     # Check if frame is not empty
-
 
     aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
     parameters = aruco.DetectorParameters_create()
@@ -59,12 +61,10 @@ while cap.isOpened():
             cv2.fillConvexPoly(org_frame, pts_dst.astype(int), 0, 16)
 
             org_frame = cv2.add(org_frame, temp)
-            org_frame = cv2.flip(org_frame, 1)
-
+            
         cv2.imshow('frame', org_frame)
 
     else:
-        frame = cv2.flip(frame, 1)
         cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
